602a0942324538722171c314992f14bc2cb31fd1
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-11 20:17:58 -0400

    Feat: added earn_permit method and tests

--- spec/registrant_spec.rb
+++ spec/registrant_spec.rb
@@ -5,13 +5,11 @@ RSpec.describe Registrant do
   before(:each) do
     @registrant_1 = Registrant.new('Bruce', 18, true )
     @registrant_2 = Registrant.new('Penny', 15 )
+    # require 'pry'; binding.pry
   end
 
   it '#registrant exists' do
     expect(@registrant_1).to be_an_instance_of(Registrant)
-  end
-
-  it '#registrant exists again' do
     expect(@registrant_2).to be_an_instance_of(Registrant)
   end
 
@@ -19,5 +17,19 @@ RSpec.describe Registrant do
     expect(@registrant_1.name).to eq("Bruce")
     expect(@registrant_1.age).to eq(18)
     expect(@registrant_1.permit?).to eq(true)
+    expect(@registrant_2.name).to eq("Penny")
+    expect(@registrant_2.age).to eq(15)
+    expect(@registrant_2.permit?).to eq(false)
+  end
+
+  it 'has license data' do
+    expect(@registrant_1.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    expect(@registrant_2.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+  end
+
+  it 'gives registrant a permit' do
+    # require 'pry'; binding.pry
+    expect(@registrant_2.earn_permit).to be(true)
+    expect(@registrant_2.permit?).to be(true)
   end
 end
