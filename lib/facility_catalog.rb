0dfbf909dcab814621bae1280f4b0019c231cc21
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-14 14:38:04 -0400

    Fix: removed comments

--- lib/facility_catalog.rb
+++ lib/facility_catalog.rb
@@ -6,16 +6,6 @@ class FacilityCatalog
 
     end
 
-    # def catalog_facilities(dmv_offices)
-    #     dmv_offices.map do |office|
-    #         office_info = {}
-    #         office_info[:name] = office[:dmv_office]
-    #         office_info[:address] = office[:address_li]
-    #         office_info[:phone] = office[:phone]
-    #         Facility.new(office_info)
-    #     end
-    # end
-
     def catalog_facilities(dmv_offices)
         dmv_offices.map do |office|
             office_info = {}
