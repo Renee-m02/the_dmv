d0f020ed7515ea4940ec2941075b80a0176c6d34
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-14 13:35:49 -0400

    Test: add more tests for the create_vehicles method

--- spec/vehicle_factory_spec.rb
+++ spec/vehicle_factory_spec.rb
@@ -17,6 +17,9 @@ RSpec.describe VehicleFactory do
             wa_data = @factory.create_vehicles(@wa_ev_registrations)
             expect(wa_data[0]).to be_an_instance_of(Vehicle)
             expect(wa_data[6]).to be_an_instance_of(Vehicle)
+            expect(wa_data[6].make).to be_a(String)
+            expect(wa_data[0].engine).to eq(:ev)
+            expect(wa_data[3].vin).to be_a(String)
         end
     end
 
