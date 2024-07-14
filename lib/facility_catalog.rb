eced542136486eb6a59bdde68aaf2b70bb575cd9
Renee Messersmith <morganrmessersmith@gmail.com>
2024-07-14 14:37:23 -0400

    Feat: got catalog_facility method to work properly

--- lib/facility_catalog.rb
+++ lib/facility_catalog.rb
@@ -6,40 +6,40 @@ class FacilityCatalog
 
     end
 
-    def catalog_facilities(dmv_offices)
-        dmv_offices.map do |office|
-            office_info = {}
-            office_info[:name] = office[:dmv_office]
-            office_info[:address] = office[:address_li]
-            office_info[:phone] = office[:phone]
-            Facility.new(office_info)
-        end
-    end
-
     # def catalog_facilities(dmv_offices)
     #     dmv_offices.map do |office|
     #         office_info = {}
-    #         if office[:dmv_office] == true
-    #             office_info[:name] = office[:dmv_office]
-    #         elsif office[:office_name] == true
-    #             office_info[:name] = office[:office_name]
-    #         elsif office[:name] == true
-    #             office_info[:name] = office[:name]
-    #         end
-    #         if office[:address_li] == true
-    #             office_info[:address] = office[:address_li]
-    #         elsif office[:street_address_line_1] == true
-    #             office_info[:address] = office[:street_address_line_1]
-    #         elsif office[:address1] == true
-    #             office_info[:address] = office[:address1]
-    #         end
-    #         if office[:phone] == true
-    #             office_info[:phone] = office[:phone]
-    #         elsif office[:public_phone_number] == true
-    #             office_info[:phone] = office[:public_phone_number]
-    #         end
+    #         office_info[:name] = office[:dmv_office]
+    #         office_info[:address] = office[:address_li]
+    #         office_info[:phone] = office[:phone]
     #         Facility.new(office_info)
     #     end
     # end
+
+    def catalog_facilities(dmv_offices)
+        dmv_offices.map do |office|
+            office_info = {}
+            if office[:dmv_office]
+                office_info[:name] = office[:dmv_office]
+            elsif office[:office_name]
+                office_info[:name] = office[:office_name]
+            elsif office[:name]
+                office_info[:name] = office[:name]
+            end
+            if office[:address_li]
+                office_info[:address] = office[:address_li]
+            elsif office[:street_address_line_1]
+                office_info[:address] = office[:street_address_line_1]
+            elsif office[:address1]
+                office_info[:address] = office[:address1]
+            end
+            if office[:phone]
+                office_info[:phone] = office[:phone]
+            elsif office[:public_phone_number]
+                office_info[:phone] = office[:public_phone_number]
+            end
+            Facility.new(office_info)
+        end
+    end
 
 end
